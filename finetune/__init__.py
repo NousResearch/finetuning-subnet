1c1c713acf5412d840c9db11cc7bbe0e4ae968b4
emozilla <emozilla@nousresearch.com>
2024-02-05 02:02:04 +0000

    update version

--- finetune/__init__.py
+++ finetune/__init__.py
@@ -15,7 +15,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 version_split = __version__.split(".")
