2bb3f491996eff36606c886795fb3bae0d00e1ff
emozilla <emozilla@nousresearch.com>
2024-02-26 14:49:04 +0000

    update model_validation.py for competitions

--- utilities/model_validation.py
+++ utilities/model_validation.py
@@ -3,20 +3,24 @@ import math
 import random
 
 from model.data import ModelId
-from transformers import LlamaForCausalLM, AutoTokenizer
+from transformers import AutoTokenizer
 from finetune.dataset import CortexSubsetLoader
 from finetune.validation import compute_losses
 from model.data import Model
 import argparse
+import constants
+import torch
+from model.model_updater import ModelUpdater
 from utilities.perf_monitor import PerfMonitor
 
 
-def load_model(model_path):
+def load_model(model_path, parameters: constants.CompetitionParameters):
     model_id = ModelId(namespace="namespace", name="name")
-    pt_model = LlamaForCausalLM.from_pretrained(
+    pt_model = parameters.architecture.from_pretrained(
         pretrained_model_name_or_path=model_path,
         local_files_only=True,
         use_safetensors=True,
+        **parameters.kwargs
     )
 
     tokenizer = AutoTokenizer.from_pretrained(
@@ -49,12 +53,41 @@ def main():
         default=400,
         help="Number of most recent Cortex samples to eval against",
     )
+    parser.add_argument(
+        "--attn_implementation",
+        help="Implementation of attention to use",
+    )
+    parser.add_argument(
+        "--dtype",
+        type=str,
+        default="bfloat16",
+        help="datatype to load model in, either bfloat16 or float16",
+    )
+    parser.add_argument(
+        "--competition_id",
+        type=str,
+        default=constants.ORIGINAL_COMPETITION_ID,
+        help="competition to validate against (use --list-competitions to get all competitions)"
+    )
+    parser.add_argument(
+        "--list_competitions",
+        action="store_true",
+        help="Print out all competitions"
+    )
     args = parser.parse_args()
+    if args.list_competitions:
+        print(constants.COMPETITION_SCHEDULE)
+        return
+    
+    competition_parameters = ModelUpdater.get_competition_parameters(args.competition_id)
+    competition_parameters.kwargs["torch_dtype"] = torch.bfloat16 if args.dtype == "bfloat16" else torch.float16
+    if args.attn_implementation:
+        competition_parameters.kwargs["attn_implementation"] = args.attn_implementation
 
-    print("Loading model")
+    print(f"Loading model for competition {args.competition_id}")
     load_model_perf = PerfMonitor("Eval: Load model")
     with load_model_perf.sample():
-        model = load_model(model_path=args.model_path)
+        model = load_model(args.model_path, competition_parameters)
     print(load_model_perf.summary_str())
 
     print("Getting latest Cortex data")
