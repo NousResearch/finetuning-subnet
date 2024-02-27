c63ef7819e3b2d59e79b0e027530f0d6a4e24d5c
emozilla <emozilla@nousresearch.com>
2024-02-27 10:36:14 -0800

    fix miner for competitions

--- neurons/miner.py
+++ neurons/miner.py
@@ -237,7 +237,7 @@ async def main(config: bt.config):
             use_wandb = True
 
     block = metagraph.block.item()
-    model_parameters = ModelUpdater.get_competition_parameters(block, config.competition_id)
+    model_parameters = ModelUpdater.get_competition_parameters(config.competition_id)
     if not model_parameters:
         raise RuntimeError(
             f"No model parameters found for block {block}"
