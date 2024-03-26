3c439f32bdb36dc5624da115adce2543745f27b1
emozilla <emozilla@nousresearch.com>
2024-03-26 15:46:37 +0000

    bump version

--- finetune/__init__.py
+++ finetune/__init__.py
@@ -15,7 +15,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-__version__ = "0.2.1"
+__version__ = "0.2.3"
 
 
 version_split = __version__.split(".")
