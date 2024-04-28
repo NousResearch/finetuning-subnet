855044b983117cc109b8b5492e25475d287ce2fb
Jeffrey Quesnelle <emozilla@nousresearch.com>
2024-04-27 22:55:43 -0400

    Update __init__.py

--- finetune/__init__.py
+++ finetune/__init__.py
@@ -15,7 +15,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 
 version_split = __version__.split(".")
